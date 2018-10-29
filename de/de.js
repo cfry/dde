55d6e2cb98bb9ace1e2c2bbf3450f15d5636e231
cfry <cfry@media.mit.edu>
2018-10-29 18:45:17 -0400

    release

--- de/de.js
+++ de/de.js
@@ -20,7 +20,6 @@ var DE = class DE {
   }
   
   static handle_infix_call(got_you, subj, vb, args) {
-        debugger
         if (got_you) { return false }
         else if(subj && args) {
         	args = args.substring(1, args.length - 1) //strip ( and )
@@ -28,19 +27,23 @@ var DE = class DE {
             let clean_args = []
             for(let arg of args_array) {clean_args.push(arg.trim()) }
             let op = DE.ops[vb]
-            let result = ""
-            if      (subj == "all" && (vb == "of")) { op = "&&" }
-            else if (subj == "one" && (vb == "of")) { op = "||" }
-            else if (!op) { return false } //not an infix call, handle with normal call code
-            else { result = subj }
-            for(let clean_arg of clean_args) {
-              result += " " + op + " " + clean_arg
+            if (!op) { return false } //not an infix call, handle with normal call code
+            else if (clean_args.length == 0) { 
+               dde_error("In Definitive English, with subject of: " + subj + 
+                         " and verb of: " + vb + 
+                         " which requires arguments but none were supplied.")
             }
-            return "(" + result + ")"
+            else { 
+            	let result = subj
+              	for(let clean_arg of clean_args) {
+                	result += " " + op + " " + clean_arg
+              	}
+             	return "(" + result + ")"
+           }
         }
   		else { return false }
 	}
-}
+} //end class DE
 
 DE.ops = {"less than": "<",
           "equals":    "==",
@@ -58,20 +61,20 @@ DE.ops = {"less than": "<",
           "remainder" : "%",
           "to the power of": "**",
 
-          "all of": "&&",
-          "one of": "||"
+          "as well as": "&&",
+          "or":   "||"
          }
 DE.peg = require("pegjs")
 DE.parser = DE.peg.generate(
 `start               = result:expr { return result }
-expr                 = before_com:comment* ws_or_not result:(maker / sentence / path / variable / undefined / number ) ws_or_not after_com:comment* 
+expr                 = before_com:comment* ws_or_not result:(maker / sentence / string / path / variable / undefined / number) ws_or_not after_com:comment* 
                        { return before_com + result + after_com }
-maker                = "make" ws (("an" / "a") ws)? subj:subject ws "with" ws args:arguments? "." { return "new " + subj + args }
+maker                = "make" ws (("an" / "a") ws)? subj:subject ws "with" ws args:arguments? sent_end { return "new " + subj + args }
 
 sentence             = result:(sent_verb_args / sent_subj_verb_args / sent_subj_verb / sent_verb) { return result }
-sent_verb_args       = vb:verb ws "with" ws args:arguments "." { return vb + args }
-sent_subj_verb_args  = subj:subject ws vb:verb ws "with" ws args:arguments "." { 
-	                         debugger;
+sent_verb_args       = vb:verb ws "with" ws args:arguments sent_end { return vb + args }
+sent_subj_verb_args  = subj:subject ws vb:verb ws "with" ws args:arguments sent_end { 
+	                         //debugger;
                              let got_you = ((subj == "you") || (subj == "You"))
                              let infix_result = DE.handle_infix_call(got_you, subj, vb, args)
                              if( infix_result) { return infix_result }
@@ -80,13 +83,22 @@ sent_subj_verb_args  = subj:subject ws vb:verb ws "with" ws args:arguments "." {
                              	return first + vb + args 
                              }
                             }
-sent_subj_verb       = subj:subject ws vb:verb ws "with" (ws undefined)? "." { return subj + "." + vb + "()" }
-sent_verb            = vb:verb ws "with" (ws undefined)? "."                 { return vb + "()"  }
+sent_subj_verb       = subj:subject ws vb:verb ws "with" (ws undefined)? sent_end { return subj + "." + vb + "()" }
+sent_verb            = vb:verb ws "with" (ws undefined)? sent_end                 { return vb + "()"  }
+sent_end             = ws_or_not "."      { return "." }
 
 subject              = result:(path / variable / number)            { return result }
 verb                 =  ` + DE.make_verb_rule_expers()    +       ` { return text() }
 arguments            = var_num1:argument var_nums:(arg_sep argument)* { 
                        let args = var_num1
+                       //debugger
+                       //get rid of trailing undefined args as would happen with DE "nothing"
+                       for(let i = (var_nums.length - 1); i >= 0; i--){
+                           if(var_nums[i][0] == "undefined") { var_nums.pop() } 
+                       }
+                       if((var_nums.length == 0) && (var_num1 == "undefined")) {
+                         args = ""
+                       }
                        for(let i = 0; i < var_nums.length; i++){
                        	  let arr = var_nums[i]
                           let prefix = ", "
@@ -102,8 +114,14 @@ argument             = arg_name:(variable ws "of" ws)? the_expr:expr {
                        }
 arg_sep  "arg_sep"   = ws_or_not ("and" / ",") ws_or_not  { return ", " }
 
+string               = ('"' [^\\"]* '"') / ("'" [^\\']* "'") {
+                         debugger;
+                         let result = text()
+                         console.log(result)
+                         return result 
+                     }
+                     
 path     "path"      = variable ("/" variable)+           { return replace_substrings(text(), "/", ".") }
-var_or_num           = variable / number                  { return text() }
 variable "variable"  = !reserved_word [a-z_]i [a-z0-9_]*  { return text() }
 
 undefined            = "nothing"  { return "undefined" }
@@ -113,10 +131,12 @@ number   "number"    = float / integer                    { return text() }
 float    "float"     = integer "." integer_sans_sign      { return text() }
 integer  "integer"   = ("+" / "-")? digits:[0-9]+         { return text() }
 integer_sans_sign    = digits:[0-9]+  { return text() }
+
 ws       "ws"        = [ \\t\\r\\n]+
 ws_or_not            = [ \\t\\r\\n]*
 comment              = "(" result:[^\\)]* ")"  { return "/*" + result.join("") + "*/" }
-`
+`,
+{trace: true}
 )
 
 
