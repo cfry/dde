1d947ca21d57e516501829fdf46863242023c9e1
cfry <cfry@media.mit.edu>
2020-11-30 23:25:16 -0500

    release

--- core/index.js
+++ core/index.js
@@ -1,5 +1,5 @@
-global.dde_version = "3.6.8"
-global.dde_release_date = "Nov 27, 2020"
+global.dde_version = "3.6.9"
+global.dde_release_date = "Nov 30, 2020"
 
 console.log("dde_version: " + global.dde_version + " dde_release_date: " + global.dde_release_date +
             "\nRead electron_dde/core/job_engine_doc.txt for how to use the Job Engine.\n")
