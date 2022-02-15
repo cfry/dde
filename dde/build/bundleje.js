3e6529c7cba394ccc75118e47c4534743db6c7ca
Damien <>
2022-02-15 14:06:22 -0800

    Include js resources for dde4
    These files were before being produced when building the server's
    served content, but they should be the functionally the same for any
    instance of the server.

---
+++ dde/build/bundleje.js
@@ -0,0 +1,10 @@
+function readyje(){
+    console.log("readyje called");
+}
+
+function testjefn(){
+    console.log("this is the testjefn talking");
+}
+
+export { readyje, testjefn };
+//# sourceMappingURL=bundleje.js.map
