f4fc701e9949ef22868fe3302f7dfdc676e25616
author <>
2023-12-29 13:46:17 -0800

    Create window

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log("Hello from Electron 👋");
+const { app, BrowserWindow } = require("electron");
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  win.loadFile("index.html");
+};
+
+app.whenReady().then(() => {
+  createWindow();
+});
