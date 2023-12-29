877f806bb0ae5a9a8707ca305f889e315a994f2e
author <>
2023-12-29 14:06:04 -0800

    Use Preload script
    https://www.electronjs.org/docs/latest/tutorial/tutorial-preload

---
+++ preload.js
@@ -0,0 +1,7 @@
+const { contextBridge } = require("electron/renderer");
+
+contextBridge.exposeInMainWorld("versions", {
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron,
+});
